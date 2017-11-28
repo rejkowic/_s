65136eed1a9c9717525ad1803c12b9e7ca98386b
Pawel Rejkowicz <pawel@rejkowicz.pl>
2017-11-28 22:17:43 +0100

    removing style.css from head

--- functions.php
+++ functions.php
@@ -117,7 +117,7 @@ add_action( 'widgets_init', '_s_widgets_init' );
  * Enqueue scripts and styles.
  */
 function _s_scripts() {
-	wp_enqueue_style( '_s-style', get_stylesheet_uri() );
+	//wp_enqueue_style( '_s-style', get_stylesheet_uri() );
 
 	wp_enqueue_script( '_s-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
 
